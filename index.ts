114b4a6bb45fd8a1a548700755433ce0a90f167d
austinpower1258 <austinwang115@gmail.com>
2023-11-30 11:57:46 -0500

    edit size constraints

--- index.ts
+++ index.ts
@@ -42,21 +42,30 @@ async function processPage(url: string, page: Page, index: number, imageOffset: 
   const downloadAndProcessImage = async (imageUrl: string, i: number) => {
     const originalImage = await downloadImage(imageUrl);
     const sizeInKB = originalImage.length / 1024;
+    console.log(sizeInKB, imageUrl);
 
-    if (sizeInKB < 10) {
-      console.log(`Skipping small image: ${imageUrl}`);
+    if (sizeInKB < 12 || sizeInKB >= 65) {
+      console.log(`Skipping bad images: ${imageUrl}`);
       return;
     }
 
     const convertedImage = await convertImageToJpeg(originalImage);
 
     if (convertedImage) {
+      console.log(convertedImage.length / 1024, imageUrl);
+      if (convertedImage.length / 1024 < 20 || convertedImage.length / 1024 >= 65) {
+        console.log(`Skipping bad images: ${imageUrl}`);
+        return;
+      }
+
       const { data, error } = await supabaseAdmin.storage.from('images').upload(`images/${crypto.randomUUID()}.jpeg`, convertedImage, { contentType: 'image/jpeg' });
 
       if (error) {
         console.error('Error uploading image:', error);
         throw error;
       }
+
+
 
       const { data: insertData, error: insertError } = await supabaseAdmin
         .from('images')
@@ -115,20 +124,22 @@ async function main() {
 
   const urls = [
     "https://www.yelp.com/biz_photos/the-luke-brasserie-new-haven?tab=food",
-    "https://www.yelp.com/biz_photos/zeneli-pizzeria-and-cucina-napoletana-new-haven-2?tab=food",
-    "https://www.yelp.com/biz_photos/noa-by-september-in-bangkok-new-haven?tab=food",
-    "https://www.yelp.com/biz_photos/menya-gumi-new-haven?tab=food",
-    "https://www.yelp.com/biz_photos/tavern-on-state-new-haven-4?tab=food",
-    "https://www.yelp.com/biz_photos/hachiroku-shokudo-and-sake-bar-new-haven?tab=food",
-    "https://www.yelp.com/biz_photos/icaru-peruvian-restaurant-new-haven?tab=food",
-    "https://www.yelp.com/biz_photos/elm-city-social-new-haven?tab=food",
-    "https://www.yelp.com/biz_photos/villa-lulu-new-haven?tab=food",
-    "https://www.yelp.com/biz_photos/september-in-bangkok-new-haven?tab=food",
-    "https://www.yelp.com/biz_photos/gioia-cafe-and-bar-new-haven?tab=food",
-    "https://www.yelp.com/biz_photos/barcelona-new-haven-new-haven-3?tab=food",
-    "https://www.yelp.com/biz_photos/olea-new-haven-2?tab=food",
-    "https://www.yelp.com/biz_photos/sherkaan-indian-street-food-new-haven-2?tab=food",
-    "https://www.yelp.com/biz_photos/tibetan-kitchen-new-haven?tab=food"
+    // "https://www.yelp.com/biz_photos/zeneli-pizzeria-and-cucina-napoletana-new-haven-2?tab=food",
+    // "https://www.yelp.com/biz_photos/noa-by-september-in-bangkok-new-haven?tab=food",
+    // "https://www.yelp.com/biz_photos/menya-gumi-new-haven?tab=food",
+    // "https://www.yelp.com/biz_photos/tavern-on-state-new-haven-4?tab=food",
+    // "https://www.yelp.com/biz_photos/hachiroku-shokudo-and-sake-bar-new-haven?tab=food",
+    // "https://www.yelp.com/biz_photos/icaru-peruvian-restaurant-new-haven?tab=food",
+    // "https://www.yelp.com/biz_photos/elm-city-social-new-haven?tab=food",
+    // "https://www.yelp.com/biz_photos/villa-lulu-new-haven?tab=food",
+    // "https://www.yelp.com/biz_photos/september-in-bangkok-new-haven?tab=food",
+    // "https://www.yelp.com/biz_photos/gioia-cafe-and-bar-new-haven?tab=food",
+    // "https://www.yelp.com/biz_photos/barcelona-new-haven-new-haven-3?tab=food",
+    // "https://www.yelp.com/biz_photos/olea-new-haven-2?tab=food",
+    // "https://www.yelp.com/biz_photos/sherkaan-indian-street-food-new-haven-2?tab=food",
+    // "https://www.yelp.com/biz_photos/tibetan-kitchen-new-haven?tab=food",
+    // "https://www.yelp.com/biz_photos/south-bay-new-haven-3?tab=food",
+    // "https://www.yelp.com/biz_photos/prime-16-new-haven?tab=food"
   ];
 
   for (let i = 0; i < urls.length; i++) {
